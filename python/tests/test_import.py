2ed1e913bed22a4f0cc03ab663c7d64e700dca80
Minh Tri <minhtri3007@hotmail.com>
2024-11-15 03:15:39 +0100

    tests: basic import and functionality test

--- python/tests/test_import.py
+++ python/tests/test_import.py
@@ -1,9 +1,11 @@
 from pyrat import PyRatEnv
 
 
-def test_import():
+def test_import() -> None:
+    """Tests that the PyRatEnv imports correctly."""
     env = PyRatEnv()
     obs = env.reset()
     actions = [0, 1]  # Up for player 1, Right for player 2
-    obs, rewards, done, truncated, info = env.step(actions)
-    print(obs)
+    done = False
+    while not done:
+        obs, rewards, done, truncated, info = env.step(actions)
